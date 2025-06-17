4070904e298f84ebcccb6409237975e4cf512eac
sZellwegerGit <105966121+sZellwegerGit@users.noreply.github.com>
2025-06-17 12:32:16 +0200

    Create reforestation-unit-crane.lua

---
+++ prototypes/reforestation-unit-crane.lua
@@ -0,0 +1,216 @@
+local hit_effects = require("__base__.prototypes.entity.hit-effects")
+local sounds = require("__base__.prototypes.entity.sounds")
+local space_age_sounds = require ("__space-age__.prototypes.entity.sounds")
+local item_sounds = require("__base__.prototypes.item_sounds")
+local space_age_item_sounds = require("__space-age__.prototypes.item_sounds")
+local item_tints = require("__base__.prototypes.item-tints")
+local item_effects = require("__space-age__.prototypes.item-effects")
+
+
+local entity = {
+	type = "agricultural-tower",
+	name = "reforestation-unit",
+	icon = "__ReforestationUnit__/graphics/reforestation-unit-icon.png",
+	flags = {"placeable-neutral", "placeable-player", "player-creation"},
+	minable = {mining_time = 0.2, result = "reforestation-unit"},
+	fast_replaceable_group = "reforestation-unit",
+	max_health = 1000,
+	corpse = "agricultural-tower-remnants",
+	dying_explosion = "agricultural-tower-explosion",
+	open_sound = sounds.mech_large_open,
+	close_sound = sounds.mech_large_close,
+	heating_energy = "50kW",
+	energy_usage = "175kW",
+	accepted_seeds = {"tree-seed"},
+	crane_energy_usage = "0kW",
+    input_inventory_size = 3,
+    output_inventory_size = 0,
+    radius_visualisation_picture =
+    {
+      filename = "__core__/graphics/white-square.png",
+      priority = "extra-high-no-scale",
+      width = 10,
+      height = 10
+    },
+    radius = 20,
+    growth_grid_tile_size = 1,
+    growth_area_radius = 0.2,
+    planting_procedure_points =
+    {
+      {0.0, 0.0, 0.75},
+      {0.0, 0.0, 0.0},
+      {0.0, 0.05, -0.05},
+      {0.0353553, 0.0353553, -0.1},
+      {0.05, 0.0, -0.15},
+      {0.0353553, -0.0353553, -0.2},
+      {0.0, -0.05, -0.25},
+      {-0.0353553, -0.0353553, -0.3},
+      {-0.05, 0.0, -0.35},
+      {-0.0353553, 0.0353553, -0.4},
+      {0.0, 0.0, -0.45},
+      {0.0, 0.0, 0.0}
+    },
+    harvesting_procedure_points =
+    {
+      {0.0, 0.0, 1.0}
+    },
+	
+	crane = {
+		speed = {
+			arm = {
+				turn_rate = 0.5,
+				extension_speed = 2.5
+			},
+			grappler = {
+				vertical_turn_rate = 0.05,
+				horizontal_turn_rate = 0.05,
+				extension_speed = 0.05,
+				allow_transpolar_movement = true
+			}
+		},
+		origin = {0, 0, 0}
+		shadow_direction = {0, 0, 0}
+		parts = {
+			{},
+			{},
+		}
+	},
+	
+	working_sound =
+	{
+	  sound =
+	  {
+		filename = "__space-age__/sound/entity/biochamber/biochamber-loop.ogg",
+		volume = 0.7,
+		audible_distance_modifier = 0.7,
+	  },
+	  max_sounds_per_prototype = 4,
+	  fade_in_ticks = 4,
+	  fade_out_ticks = 20
+	},
+	
+    central_orienting_sound =
+    {
+      sound = nil,
+      stopped_sound = nil,
+    },
+    central_orienting_sound_source = "hub",
+    arm_extending_sound =
+    {
+      sound = nil,
+      stopped_sound = nil,
+    },
+    arm_extending_sound_source = "arm_central_joint",
+    grappler_orienting_sound =
+    {
+      sound = nil,
+      stopped_sound = nil,
+    },
+    grappler_orienting_sound_source = "grappler-hub",
+    grappler_extending_sound =
+    {
+      sound = nil,
+      stopped_sound = nil,
+    },
+    grappler_extending_sound_source = "grappler-hub",
+    planting_sound = nil,
+    harvesting_sound = nil,
+	
+	resistances =
+	{
+	  {
+		type = "fire",
+		percent = 50
+	  }
+	},
+	collision_box = {
+		--{-1.2, -1.2},
+		--{1.2, 1.2}
+		{-2.0, -2.0},
+		{2.0, 2.0}
+	},
+	selection_box = {
+		{-2.0, -2.0},
+		{2.0, 2.0}
+	},
+    drawing_box = {
+		{-2.0, -2.0},
+		{2.0, 2.0}
+    },
+	collision_mask = {layers={item=true, object=true, player=true, water_tile=true, elevated_rail=true, is_object=true, is_lower_object=true}},
+	surface_conditions =
+	{
+	  {
+		property = "pressure",
+		min = 1000,
+		max = 2000
+	  }
+	},
+	damaged_trigger_effect = hit_effects.entity(),
+	energy_source =
+	{
+	  type = "electric",
+	  usage_priority = "secondary-input",
+	  emissions_per_minute = { spores = 40 }
+	},
+	circuit_connector = nill,
+	-- circuit_wire_max_distance = 30,
+	graphics_set =
+	{
+	  animation =
+	  {
+		layers = {
+		{
+		  draw_as_shadow = true,
+		  filename = "__ReforestationUnit__/graphics/reforestation-unit-shadow.png",
+		  priority = "high",
+		  width = 303,
+		  height = 128,
+		  frame_count = 1,
+		  line_length = 1,
+		  repeat_count = 1,
+		  animation_speed = 0.5,
+		  scale = 1.0,
+		  shift = {2.8, 0.08}
+		},
+		{
+		  priority = "high",
+		  width = 128,
+		  height = 335,
+		  frame_count = 1,
+		  animation_speed = 0.5,
+		  scale = 1.0,
+		  shift = {0, -2.8},
+		  stripes =
+		  {
+			{
+			  filename = "__ReforestationUnit__/graphics/reforestation-unit-blured.png",
+			  width_in_frames = 1,
+			  height_in_frames = 1,
+			}
+		  },
+		},
+	  },
+	  recipe_not_set_tint = { primary = {r = 0.6, g = 0.6, b =  0.5, a = 1}, secondary = {r = 0.6, g =  0.6, b = 0.5, a = 1} },
+	  working_visualisations = nil
+	}
+  },
+}
+
+local item = {
+	type = "item",
+	name = "reforestation-unit",
+	icon = "__ReforestationUnit__/graphics/reforestation-unit-icon.png",
+	subgroup = "agriculture",
+	order = "b[agricultural-tower]",
+	inventory_move_sound = item_sounds.mechanical_large_inventory_move,
+	pick_sound = item_sounds.mechanical_large_inventory_pickup,
+	drop_sound = item_sounds.mechanical_large_inventory_move,
+	place_result = "reforestation-unit",
+	stack_size = 20,
+	default_import_location = "nauvis"
+}
+
+data:extend{
+	item, entity
+}
